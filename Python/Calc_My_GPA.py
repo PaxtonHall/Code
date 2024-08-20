4882ccebccc79e97ac9209e2f7bfad3fe28af50a
PaxtonHall <w1783876561@gmail.com>
2024-08-20 20:14:59 +0800

    1

--- Python/Calc_My_GPA.py
+++ Python/Calc_My_GPA.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+A_PLUS = 4.3
+A = 4.0
+A_MINUS = 3.7
+
 Credit = np.array([3 , 3 , 2 , 3 , 3 , 3])
-Grade = np.array([4 , 3.7 , 4 , 4.3 , 4.3 , 4])
+Grade = np.array([A , A_MINUS , A , A_PLUS , A_PLUS , A])
+
 print(f"GPA: {(Credit * Grade).sum() / Credit.sum():.2f}")
